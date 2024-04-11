fe4477bbdf400e958b51de44b5c3e1f66dbac055
DevTae <dev.taehyeon.kim@gmail.com>
2024-04-12 00:23:35 +0900

    Init project

---
+++ src/test/java/kr/co/dbinc/BackEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dbinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
